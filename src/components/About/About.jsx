e728603a0ec722114d99f9ee33de048cf95bbe0f
olanrewaju adebayo <adebayoolanrewaju970@gamail.com>
2023-12-02 03:36:04 +0100

    file parth corrected

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import { getImageUrl } from '../../utils';
 import React from 'react';
-import styles from "./About.module.css";
+import styles from "./about.module.css";
 
 export const About = () => {
     
